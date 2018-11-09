ce500c2a2d8e3d3d3903242b472ddd3a896806fd
jatinkrmalik <jatinkrmalik@gmail.com>
2018-11-10 00:21:50 +0530

    Remove hello world

--- backend/server.py
+++ backend/server.py
@@ -8,23 +8,22 @@ from models import LowPolyGenerator
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
 @app.route('/generate')
 def generate_default():
-    points = LowPolyGenerator(center_cords=(1920//2,1080//2)).generate()
+    points = LowPolyGenerator(center_cords=(1920//2, 1080//2)).generate()
     return Response(
         response=json.dumps({'plotMe': points}),
         status=200,
         content_type='application/json'
     )
 
+
 @app.route('/generate/<int:width>/<int:height>')
 def generate_shape(width, height):
     try:
-        points = LowPolyGenerator(center_cords=(width//2,height//2)).generate()
+        points = LowPolyGenerator(
+            center_cords=(width//2, height//2)).generate()
     except ValueError as e:
         response_json = json.dumps({
             'error': e.args,
@@ -40,6 +39,7 @@ def generate_shape(width, height):
         status=status_code,
         content_type='application/json'
     )
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9090, debug=True)
